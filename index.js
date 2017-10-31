79ef37d0f97f92113621e3ddcb515273fb83a167
Matheus Rezende <matheus.rezende10@gmail.com>
2017-10-31 21:04:49 -0200

    changed the import from React.PropTypes to 'prop-types' package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Image, Platform, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
 
 const IMG_CENTER_MODE = Platform.OS === 'ios' ? 'center' : 'contain';
